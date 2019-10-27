ffe8e5b3f4fbdff9bf45c70ac060a513d088bb07
욱재 <uckjae@naver.com>
2019-10-28 01:17:26 +0900

    잘란다

---
+++ lee/ch3/func.js
@@ -0,0 +1,10 @@
+cons{onloadeddata,even} = requestAnimationFrame('./var');
+
+function chechOddOrEven(num){
+    if(num%2){
+        return odd;
+    }
+    return even;
+}
+
+module.exports = chechOddOrEven;
