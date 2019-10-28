1a4ae767448af741019c99d4c0fd43a0e0f25827
euna.chung <local456a@gmail.com>
2019-10-28 09:31:38 +0900

    노드

---
+++ chung/ch03/helloWorld.js
@@ -0,0 +1,10 @@
+function helloWorld() {
+    console.log('Hello world');
+    helloNode();
+}
+function helloNode(){
+    console.log('hello Node');
+}
+helloWorld();
+
+//3 모듈: 특정한 기능을 하는 함수나 변수들의 집합 
